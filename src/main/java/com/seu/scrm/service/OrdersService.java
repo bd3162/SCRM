c903860e5be69fcec3d1f3869d9e007fe1ba70e0
Eyre Young <yudongyang1996@gmail.com>
2019-03-14 21:26:19 +0800

    修改Service

--- src/main/java/com/seu/scrm/service/OrdersService.java
+++
@@ -1,9 +0,0 @@
-package com.seu.scrm.Service;
-
-import com.seu.scrm.Entity.Orders;
-
-import java.util.List;
-
-public interface OrdersService {
-    List<Orders> selectByUserId(String user_id);
-}
--- src/main/java/com/seu/scrm/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.seu.scrm.Service;
-
-import com.seu.scrm.Entity.Product;
-
-public interface ProductService {
-    public Product selectProductById(String  asin);
-}
